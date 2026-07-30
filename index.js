9f79e8ae0a3de410ff1af7c3d62c8d6c174a73e4
aknotiyal23-droid <aknotiyal2005@gmail.com>
2026-07-30 13:07:58 +0530

    let and var

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+//let and var keywords are used to declare variables in JavaScript. The main difference between them is their scope and hoisting behavior.
+var a=23
+console.log("value of a is "+a)
+if(a>10){
+    var a=45
+    console.log("value of a listed in if block is "+a)
+}
+console.log("value of a after if block is "+a) // value of a after if block is 45 because var has function scope, so the value of a is updated in the entire function scope.
